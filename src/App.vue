3fe79805367e4f3d7ca602ef0374e02e33b92239
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-05 13:29:38 -0300

    FEAT: Contador criado

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,23 @@
 <script setup>
 import { ref } from 'vue'
-import api from './plugins/api';
-
-const username = ref('');
-const password = ref('');
-const error = ref('');
-const isLoading = ref(false);
-const ongs = ref();
-const requestUrls = {
-  token: '/token/',
-  organizations: '/organizacoes/',
-
+const contador = ref(0)
+const increment = () => {
+  contador.value++
 }
-const login = async () => {
-  isLoading.value = true
-  error.value = ''
-
-  try {
-    const response = await api.post(requestUrls.token, {
-      username: username.value,
-      password: password.value
-    })
-
-    const { access, refresh } = response.data
-    localStorage.setItem('accessToken', access)
-    localStorage.setItem('refreshToken', refresh)
-
-    console.log('Login bem sucedido')
-    console.log(localStorage.getItem('accessToken'))
-  } catch (err){
-    error.value = 'Usuário ou senha inválidos'
-    console.log(err)
-  } finally {
-    isLoading.value = false
-    localStorage.clear()
-  }
-}
-const getOngs = async () => {
-  const data = await api.get(requestUrls.organizations, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-    }
-  });
-  ongs.value = data.data
+const decrement = () => {
+  contador.value--
 }
 </script>
 
 <template>
-   <h1 class="text-center ">
-      Hello world!
-  </h1>
-  <form @submit.prevent="login">
-    <input v-model="username" placeholder="Usuario" required class="border-1 border-cyan-200 rounded-2xl w-50 p-2 text-2xl bg-[url(bart.png)]">
-    <input v-model="password" type="password" placeholder="Senha">
-    <button type="submit" :disabled="isLoading">Entrar</button>
-    <p v-if="error">{{ error }}</p>
-    <button @click="getOngs" type="button">Carregar ongs</button>
-    <p v-for="ong in ongs" :key="ong.id">
-      {{ ong.nome }} 
-    </p>
-  </form>
+  <section class="flex flex-col items-center gap-10">
+    <h1 class="text-4xl">Contador vue</h1>
+    <div class="flex items-center gap-10">
+      <button class="text-xl border rounded-2xl px-4 py-2 hover:bg-white cursor-pointer" @click="increment">+</button>
+      <p>{{ contador }}</p>
+      <button class="text-xl border rounded-2xl px-4 py-2 hover:bg-white cursor-pointer" @click="decrement">-</button>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
